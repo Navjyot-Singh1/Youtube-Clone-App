38c1e287c6ceed9dff9fbbe40c2fe4bd6108e547
drumbum90 <68716941+drumbum90@users.noreply.github.com>
2023-01-16 18:49:37 +0530

    Frontend Completed

---
+++ src/components/Feed.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Sidebar, Videos } from "./";
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sm: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022 Navjyot Singh
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New
+          <span style={{ color: "#FC1503" }}> Videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
